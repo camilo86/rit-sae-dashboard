c3fad64b5567b26cf932c8dd1a202057cfe45bf6
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-09-09 23:30:26 -0400

    first commit

---
+++ src/rit/sae/dashboard/RitSaeDashboard.java
@@ -0,0 +1,16 @@
+package rit.sae.dashboard;
+
+/**
+ *
+ * @author Camilo Gonzalez
+ */
+public class RitSaeDashboard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
