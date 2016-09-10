39adc097e366cdf0a6343998b2be38568913ba43
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-09-10 10:52:33 -0400

    Opened device. should be ready to test

--- src/rit/sae/dashboard/RitSaeDashboard.java
+++ src/rit/sae/dashboard/RitSaeDashboard.java
@@ -1,16 +1,32 @@
 package rit.sae.dashboard;
 
+import com.digi.xbee.api.XBeeDevice;
+import com.digi.xbee.api.exceptions.XBeeException;
+import rit.sae.dashboard.dataListeners.LaserRecieverListener;
+
 /**
  *
  * @author Camilo Gonzalez and Jon Turinese
  */
 public class RitSaeDashboard {
 
+    private static final String PORT = "COM1";
+    private static final int BAUD_RATE = 9600;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        XBeeDevice myDevice = new XBeeDevice(PORT, BAUD_RATE);
+		
+        try {
+            myDevice.open();
+            myDevice.addDataListener(new LaserRecieverListener());		
+            System.out.println("\n>> Waiting for data...");
+            
+	} catch (XBeeException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
-    
 }
